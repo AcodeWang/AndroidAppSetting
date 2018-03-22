c26fc6b9ba052159a2e5c834f9fbc6e7d50bbf6f
ACodeWang <gsdwyane@gmail.com>
2018-03-22 16:55:13 +0100

    take photo with android camera development

--- app/src/main/java/com/piinktecknology/chenyu/androidwiiovision/CameraActivity.kt
+++ app/src/main/java/com/piinktecknology/chenyu/androidwiiovision/CameraActivity.kt
@@ -1,5 +1,79 @@
 package com.piinktecknology.chenyu.androidwiiovision
 
+import android.provider.MediaStore
+import android.content.Intent
+import android.os.Bundle
+import android.os.Environment
+import android.support.v7.app.AppCompatActivity
+import android.support.v4.content.FileProvider
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+
+
 /**
  * Created by chenyu on 22/03/2018.
  */
+
+class CameraActivity : AppCompatActivity(){
+
+    companion object {
+        lateinit var photoFile:File
+    }
+
+    val REQUEST_TAKE_PHOTO= 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        takePhoto()
+    }
+
+    fun takePhoto() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+
+            try{
+                photoFile = createPhotoFile()
+                val photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile)
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+            }
+            catch (e: IOException){
+
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK){
+            takePhoto()
+        }
+        else{
+
+        }
+    }
+
+    @Throws(IOException::class)
+    fun createPhotoFile(): File {
+        // Create an image file name
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageFileName = "JPEG_" + timeStamp + "_"
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val image = File.createTempFile(
+                imageFileName, /* prefix */
+                ".jpg", /* suffix */
+                storageDir      /* directory */
+        )
+        // Save a file: path for use with ACTION_VIEW intents
+        var mCurrentPhotoPath = image.getAbsolutePath()
+        return image
+    }
+}
